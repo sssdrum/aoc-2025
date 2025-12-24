5f1092a34c79f8ca79e59757e0bd831f03b16efb
Luigi Di Paolo <luigi.dipaolo02@gmail.com>
2025-12-24 14:32:21 +0000

    Add day 1 part 1

--- day01/main.go
+++ day01/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/sssdrum/aoc-2025/utils"
+)
+
+const (
+	START = 0
+	END   = 99
+)
 
 func main() {
-	fmt.Println("Hello from day 1")
+	part1()
+}
+
+func part1() {
+	curr := 50
+	res := 0
+	lines := utils.ReadLines("./input.txt")
+	for _, l := range lines {
+		dir, tok := l[0], l[1:]
+		steps, err := strconv.Atoi(tok)
+		utils.CheckErr(err)
+
+		if dir == 'R' {
+			curr = (curr + steps) % (END + 1)
+		} else {
+			curr = ((curr - steps) + END + 1) % (END + 1)
+		}
+
+		if curr == 0 {
+			res++
+		}
+	}
+
+	fmt.Println(res)
 }
