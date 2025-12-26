0e55c42c66af2592d2eda1f989cf642dc84ce5e4
Luigi Di Paolo <luigi.dipaolo02@gmail.com>
2025-12-26 18:29:41 +0000

    Add day 3 part 2

--- day03/main.go
+++ day03/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -28,4 +29,38 @@ func part1() {
 		res += currMax
 	}
 	fmt.Println(res)
+}
+
+func part2() {
+	lines := utils.ReadLines("./input.txt")
+	res := 0
+	lineLen := len(lines[0])
+	for _, line := range lines {
+		digits, need, start, end, s := lineLen, 12, 0, 0, ""
+		for need > 0 {
+			end = start + digits - need
+			maxFound, pos := maxIntInStr(line[start : end+1])
+			s += strconv.Itoa(maxFound)
+			start += pos + 1
+			need--
+			digits = len(line) - start
+		}
+		n, _ := strconv.Atoi(s)
+		res += n
+	}
+	fmt.Println(res)
+}
+
+// Assumes the input is composed of only positive integers. Returns the maximum
+// integer found, with its position in the string
+func maxIntInStr(s string) (int, int) {
+	maxFound, pos := -1, 0
+	for i, r := range s {
+		n, _ := strconv.Atoi(string(r))
+		if n > maxFound {
+			maxFound = n
+			pos = i
+		}
+	}
+	return maxFound, pos
 }
