2a0da82d7339b4eb9401794698ffac665a853793
Luigi Di Paolo <luigidipa@VFIEVOX3.Router>
2026-01-08 19:53:14 +0000

    Add day 6 part 2

--- day06/main.go
+++ day06/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -37,6 +38,51 @@ func part1() {
 	res := 0
 	for _, t := range tots {
 		res += t
+	}
+	fmt.Println(res)
+}
+
+func part2() {
+	lines := utils.ReadLines("./input.txt")
+
+	// Add whitespace to shorter lines
+	max_len := 0
+	for _, l := range lines {
+		max_len = max(max_len, len(l))
+	}
+	for i, l := range lines {
+		if len(l) < max_len {
+			lines[i] = lines[i] + strings.Repeat(" ", max_len-len(l))
+		}
+	}
+
+	// Kinda horrible but it does the job
+	num_lines := len(lines)
+	last_line := string(lines[num_lines-1])
+	res := 0
+	for i := range len(last_line) {
+		if last_line[i] != ' ' {
+			op := last_line[i]
+			tot := 0
+			if op == '*' {
+				tot = 1
+			}
+			j := i
+			for j < max_len-1 && last_line[j+1] == ' ' || j == max_len-1 {
+				s := ""
+				for _, l := range lines[0 : num_lines-1] {
+					s += string(l[j])
+				}
+				j++
+				n, _ := strconv.Atoi(strings.TrimSpace(s))
+				if op == '*' {
+					tot *= n
+				} else {
+					tot += n
+				}
+			}
+			res += tot
+		}
 	}
 	fmt.Println(res)
 }
